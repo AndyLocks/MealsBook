b10d365179f8a72a4b4e2f9627243409af2db5a2
Илья <112815007+AndyLocks@users.noreply.github.com>
2023-03-16 15:40:28 +0100

    Add files via upload

---
+++ Test/Test/TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestApp.swift
+//  Test
+//
+//  Created by Galina Makarova on 15.03.23.
+//
+
+import SwiftUI
+
+@main
+struct TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
